a30a80dc3b5e8d6b7e860066f213776ef8408264
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-02-17 14:10:54 -0300

    Comentarios y 3 clases nuevas.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-          Se instancia una Persona y se llama a varios de sus métodos como prueba.
-         */
+        // Se instancia una Persona y se llama a varios de sus métodos como prueba.
         Person person1 = new Person();
         person1.setName("Juan");
         person1.setLastName1("De Los");
